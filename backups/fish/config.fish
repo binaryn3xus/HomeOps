9e7f4a807b1f571131f07fcb15a9712b1ec8d3e0
joshuagarrison27 <joshuagarrison27@gmail.com>
2024-03-10 22:11:17 +0000

    Everything sucks and life is hard. REBOOT!

--- backups/fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-direnv hook fish | source
-alias k='kubectl'
-
-# Set PATH, MANPATH, etc., for Homebrew.
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
--- backups/fish/functions/k.fish
+++
@@ -1,7 +0,0 @@
-function k --wraps=kubectl --description 'kubectl shorthand'
-    if type -q kubecolor
-        kubecolor $argv
-    else
-        kubectl $argv
-    end
-end
